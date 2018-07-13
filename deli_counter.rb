423464b0f1b4f4c37dedbb80a758d68049006d79
Karla R <karla.y.reyes12@gmail.com>
2018-07-13 07:30:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,5 +6,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome #{name}. You are currently number #{x} in line."
+  puts "Welcome #{name}. You are currently number #{x + 1} in line."
 end
